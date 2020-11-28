4c603a0a2adefe9fb8ea42b95b43b78652d81f18
Taylor Vo <72040752+RolyatVo@users.noreply.github.com>
2020-11-27 19:38:19 -0800

    Update authenticate.js
    Added Error handling when user inputs a email that has not signed up or a invalid email or password.

--- Backend/authenticate.js
+++ Backend/authenticate.js
@@ -4,4 +4,32 @@ const signupForm = document.querySelector('#signup-form');
 
 signupForm.addEventListener('submit', (e) => { 
   e.preventDefault() 
+
+  //Getting info from the user
+  const userEmail = signupForm['user-email'].value;
+  const userPassword = signupForm['user-password'].value; 
+
+  //Logging the User in.
+  auth.signInWithEmailAndPassword(userEmail, userPassword).then(cred => { 
+    
+    signupForm.reset(); 
+  }).catch(function(error) { 
+    
+    var errorUserNotfoundCodeMessage = "Email is not signed up";
+    var errorInvalidEmailPasswordMessaage = "Wrong Email or Password" 
+
+
+    // Error handing on sign-in 
+    if (error.code === "auth/user-not-found") { 
+      alert(errorUserNotfoundCodeMessage); 
+    }
+    else if(error.code === "auth/invalid-email" || error.code === "auth/wrong-password") { 
+      alert(errorInvalidEmailPasswordMessaage);
+    }
+    else { 
+      alert(error.message); 
+    }
+
+   
+  })
 })
