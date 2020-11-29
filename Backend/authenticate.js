a34e4257ae4c0b93ee3b449a13a57d0b352a8b4a
Taylor Vo <72040752+RolyatVo@users.noreply.github.com>
2020-11-28 19:06:39 -0800

    Added different firebase, added where user can create account
    Added functions where user can create an account with their name and password.

--- Backend/authenticate.js
+++ Backend/authenticate.js
@@ -1,45 +1,102 @@
 // Singing up
 
+
 const signinForm = document.querySelector('#signin-form');
-const singupForm = document.querySelector('#signup-form')
+const signupForm = document.querySelector('#signup-form');
+
+
 
 auth.onAuthStateChanged(user => { 
   if (user) { 
     console.log("user logged in: ", user);
+    alert("Welcome " + user.displayName + "!"); 
   } else { 
     console.log("user is logged out");
   }
 });
 
+if(signinForm != null) { 
+  signinForm.addEventListener('submit', (e) => { 
+    e.preventDefault() 
 
-signinForm.addEventListener('submit', (e) => { 
-  e.preventDefault() 
+    //Getting info from the user
+    const userEmail = signinForm['user-email'].value;
+    const userPassword = signinForm['user-password'].value; 
 
-  //Getting info from the user
-  const userEmail = signinForm['user-email'].value;
-  const userPassword = signinForm['user-password'].value; 
+    //Logging the User in.
+    auth.signInWithEmailAndPassword(userEmail, userPassword).then(cred => { 
+      
+      signinForm.reset(); 
+    }).catch(function(error) { 
+      
+      var errorUserNotfoundCodeMessage = "Email is not signed up";
+      var errorInvalidEmailPasswordMessaage = "Wrong Email or Password" 
 
-  //Logging the User in.
-  auth.signInWithEmailAndPassword(userEmail, userPassword).then(cred => { 
+
+      // Error handing on sign-in 
+      if (error.code === "auth/user-not-found") { 
+        alert(errorUserNotfoundCodeMessage); 
+      }
+      else if(error.code === "auth/invalid-email" || error.code === "auth/wrong-password") { 
+        alert(errorInvalidEmailPasswordMessaage);
+      }
+      else { 
+        alert(error.message); 
+      }
+
     
-    signinForm.reset(); 
-  }).catch(function(error) { 
+    })
+  });
+}
+
+
+
+if (signupForm != null) { 
+  signupForm.addEventListener('submit', (e) => { 
+    e.preventDefault() 
+
+    //Getting the information
+
+    const userEmail = signupForm['user-email'].value; 
+    const userPassword = signupForm['user-password'].value; 
+    const userFirstName = signupForm['first-name'].value; 
+    const userLastname = signupForm['last-name'].value; 
+    const userConfirmPassword = signupForm['user-confirm'].value;
     
-    var errorUserNotfoundCodeMessage = "Email is not signed up";
-    var errorInvalidEmailPasswordMessaage = "Wrong Email or Password" 
 
+    // Confirming the user's password. 
+    if(userPassword === userConfirmPassword) { 
+      auth.createUserWithEmailAndPassword(userEmail, userPassword).then( cred => {  
+        //Once we created the user, now we update the information about the user. 
+        if(cred) { 
+          cred.updateProfile({ 
+            displayName: userFirstName + " " + userLastname
+          }).then()
+        }
+        signupForm.reset();
+        // Error Handling
+      }).catch(function(error) { 
+        var errorCode = error.code; 
+        var errorMessage = error.message; 
 
-    // Error handing on sign-in 
-    if (error.code === "auth/user-not-found") { 
-      alert(errorUserNotfoundCodeMessage); 
-    }
-    else if(error.code === "auth/invalid-email" || error.code === "auth/wrong-password") { 
-      alert(errorInvalidEmailPasswordMessaage);
+        if (errorcode === "auth/email-already-in-use") { 
+          alert("Email is already in use!!"); 
+        }
+        else if (errorcode === "auth/invalid-email") { 
+          alert("Email is not valid."); 
+        }
+        else if (errorcode === "auth-weakpassword") { 
+          alert("Weak password."); 
+        }
+        else { 
+          alert(errorMessage);
+        }
+        console.log(error); 
+        })
+    
     }
     else { 
-      alert(error.message); 
+      alert('Passwords do not match'); 
     }
-
-   
-  })
-})
+  });
+}
