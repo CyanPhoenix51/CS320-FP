6118e84dd771316146c7f8ceac40b389185428c6
Flashlock <ajamesbetts1998@gmail.com>
2020-11-29 15:41:20 -0800

    I can draw vertices

--- graph_theorists_sketchpad/src/Sketch.js
+++ graph_theorists_sketchpad/src/Sketch.js
@@ -7,23 +7,29 @@ export default class Sketch extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            vertices: [
-                {
-                    id: 0,
-                    x: 250,
-                    y: 250
-                }
-            ]
+            vertices: [],
+            vertexIDCount: 0
         }
     }
 
     render(){
         return(
-          <div id='pad'>
+          <div id='pad' onClick={this.drawVertex.bind(this)}>
               {this.state.vertices.map((vertex) => (
                   <Vertex key={vertex.id} x={vertex.x} y={vertex.y}/>
               ))}
           </div>
         );
+    }
+
+    drawVertex(e) {
+        const vertex = {
+            id: this.state.vertexIDCount++,
+            x: e.clientX,
+            y: e.clientY
+        }
+
+        this.state.vertices.push(vertex);
+        this.setState(this.state);
     }
 }
