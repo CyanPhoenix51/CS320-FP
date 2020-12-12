dd033d843ff490e529a1b87518979c3e7886359d
Flashlock <ajamesbetts1998@gmail.com>
2020-12-12 12:25:51 -0800

    Bug Fix: Sketchbook wasn't displaying
    2 mystery cookies appeared, I don't know where they came from, but I'm just expiring them when the sketchbook tries to render

--- graph_theorists_sketchpad/src/Sketchbook.js
+++ graph_theorists_sketchpad/src/Sketchbook.js
@@ -3,7 +3,13 @@ import SavedSketch from "./SavedSketch";
 
 export default class Sketchbook extends React.Component {
   render() {
-    let sketches = document.cookie.split(';');
+    //don't know where these came from, but they gotta go.
+    document.cookie='_ga=;expires=Thu, 18 Dec 2013 12:00:00 UTC';
+    document.cookie='_ga_PTLBR59D27=;expires=Thu, 18 Dec 2013 12:00:00 UTC';
+
+    let sketches = decodeURIComponent(document.cookie);
+    sketches = sketches.split(';');
+    console.log(sketches);
     //get rid of not needed parts
     let x = [];
     for (let i = 0; i < sketches.length; i++) {
