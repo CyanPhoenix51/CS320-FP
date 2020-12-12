3a069f6bc8d4cdb4b7fbafdd4c4941b306faf6bf
Flashlock <ajamesbetts1998@gmail.com>
2020-12-12 12:28:19 -0800

    Sketches must have a name to be saved

--- graph_theorists_sketchpad/src/index.js
+++ graph_theorists_sketchpad/src/index.js
@@ -5,45 +5,48 @@ import Sketchbook from "./Sketchbook";
 
 const e=React.createElement;
 
-class App extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state={
-            currentView: 'sketchPad',
-            currentSketch: null
-        }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentView: 'sketchPad',
+      currentSketch: null
     }
+  }
 
-    render() {
-        switch (this.state.currentView){
-            case "sketchBook":
-                return <Sketchbook loadSketch={this.loadSketch}/>
-            case 'sketchPad':
-                return <Sketchpad saveSketch={this.saveSketch}/>
-            default:
-                return <h1>Ooga Booga</h1>
-        }
+  render() {
+    switch (this.state.currentView) {
+      case "sketchBook":
+        return <Sketchbook loadSketch={this.loadSketch}/>
+      case 'sketchPad':
+        return <Sketchpad saveSketch={this.saveSketch}/>
+      default:
+        return <h1>Ooga Booga</h1>
     }
+  }
 
-    loadSketch=(sketch)=> {
-      //change view
-      const state = this.state;
-      state.currentView = 'sketchPad';
-      document.cookie = 'loadSketch=' + JSON.stringify(sketch);
-      this.setState(state);
-    }
+  loadSketch = (sketch) => {
+    //change view
+    const state = this.state;
+    state.currentView = 'sketchPad';
+    document.cookie = 'loadSketch=' + JSON.stringify(sketch);
+    this.setState(state);
+  }
 
-    saveSketch=(sketch)=> {
-        //change view
-        const state = this.state;
-        state.currentView = 'sketchBook';
+  saveSketch = (sketch) => {
+    //create a cookie for the sketch
+    const s = JSON.parse(sketch);
+    if (!s.name)
+      return;
+    const name = s.name;
+    document.cookie = name + '=' + sketch;
 
-        //create a cookie for the sketch
-        const s=JSON.parse(sketch);
-        const name=s.name;
-        document.cookie=name+'='+sketch;
-        this.setState(state);
-    }
+    //change view
+    const state = this.state;
+    state.currentView = 'sketchBook';
+
+    this.setState(state);
+  }
 }
 
 //const domContainer = document.querySelector('#app_container');
