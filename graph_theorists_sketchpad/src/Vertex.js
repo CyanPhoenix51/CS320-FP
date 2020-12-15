3d8a7f85350b36dabd305bb227fada5c972926e9
NamiSaya <73250718+NamiSaya@users.noreply.github.com>
2020-12-14 19:15:18 -0800

    SketchPad has colors now

--- graph_theorists_sketchpad/src/Vertex.js
+++ graph_theorists_sketchpad/src/Vertex.js
@@ -4,15 +4,14 @@ import './styles/main.css';
 //Each vertex that is on the pad
 export default class Vertex extends React.Component {
   render() {
-    const { id, x, y, isSelected, borderRadius, selectionColor, displayVertexData, color, isHovering } = this.props.vertex;
+    const { id, x, y, isSelected, borderRadius, selectionColor, displayVertexData, } = this.props.vertex;
     return (
         <div className='vertex' style={
           {
             top: isSelected ? y - borderRadius + 'px' : y + 'px',
             left: isSelected ? x - borderRadius + 'px' : x + 'px',
             border: isSelected ? borderRadius + 'px ' + selectionColor : null,
-            // for some reason I have to use pink, I can't use the selection color
-            background: isHovering ? 'pink' : color
+            
           }
         } onClick={this.props.selectElement.bind(this, true, id)}
              onMouseEnter={this.props.mouseEnterElement.bind(this, true, id)}
